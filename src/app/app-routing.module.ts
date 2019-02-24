9a0b04f7b93c2bde9872b34ecaddc13ef86d316c
Daniel Castro <34897196+danicastro95@users.noreply.github.com>
2019-02-25 00:41:51 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'streams', component: ContentComponent },
+  { path: 'games', component: ContentComponent },
+  { path: '**', component: ContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
